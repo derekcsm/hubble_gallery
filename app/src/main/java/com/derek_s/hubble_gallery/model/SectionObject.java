0772b262458e6a03334f1383f5a31d6702836da2
Pedro <pedrovictor_caldas@hotmail.com>
2015-11-30 09:23:30 -0300

    Using inheritance in SectionObject

--- app/src/main/java/com/derek_s/hubble_gallery/model/SectionObject.java
+++ app/src/main/java/com/derek_s/hubble_gallery/model/SectionObject.java
@@ -3,39 +3,9 @@ package com.derek_s.hubble_gallery.model;
 /**
  * Created by dereksmith on 15-03-05.
  */
-public class SectionObject {
+public class SectionObject extends SectionChildObject{
 
-    private String sectionTitle;
-    private String query;
     private boolean isExpandable;
-
-    /**
-     * @return The sectionTitle
-     */
-    public String getSectionTitle() {
-        return sectionTitle;
-    }
-
-    /**
-     * @param sectionTitle The section_title
-     */
-    public void setSectionTitle(String sectionTitle) {
-        this.sectionTitle = sectionTitle;
-    }
-
-    /**
-     * @return The query
-     */
-    public String getQuery() {
-        return query;
-    }
-
-    /**
-     * @param query The query
-     */
-    public void setQuery(String query) {
-        this.query = query;
-    }
 
     public void setExpandable(boolean expandable) {
         this.isExpandable = expandable;
