8b233e95fbbceb4550d45598c11eb30ab3123d90
derekcsm <derekcraigsmith@gmail.com>
2016-03-27 00:23:47 -0700

    cleanup / fix Gradle files, update libs, add multiDex
    the usual.
    
    also some misc cleanup formatting edits

--- app/src/main/java/com/derek_s/hubble_gallery/utils/Animation/SquareFlipper.java
+++ app/src/main/java/com/derek_s/hubble_gallery/utils/Animation/SquareFlipper.java
@@ -10,84 +10,81 @@ import android.view.animation.Transformation;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 
-/**
- * Created by dereksmith on 15-04-13.
- */
 public class SquareFlipper {
-    private View flippingView;
-    private FlipAnimator flipAnimator;
-    boolean stop = false;
-    boolean justStarted = false;
+  private View flippingView;
+  private FlipAnimator flipAnimator;
+  boolean stop = false;
+  boolean justStarted = false;
 
-    public void startAnimation(final View view) {
-        if (flipAnimator != null) {
-            flipAnimator.cancel();
-            flipAnimator = null;
-        }
-        stop = false;
-        justStarted = true;
-        flippingView = view;
-        flippingView.setVisibility(View.VISIBLE);
-        flippingView.setAlpha(0);
+  public void startAnimation(final View view) {
+    if (flipAnimator != null) {
+      flipAnimator.cancel();
+      flipAnimator = null;
+    }
+    stop = false;
+    justStarted = true;
+    flippingView = view;
+    flippingView.setVisibility(View.VISIBLE);
+    flippingView.setAlpha(0);
 
-        flipAnimator = new FlipAnimator();
-        flipAnimator.setDuration(600);
-        flipAnimator.setRepeatCount(-1);
-        flipAnimator.setFillAfter(true);
-        flipAnimator.setInterpolator(new DecelerateInterpolator());
-        view.startAnimation(flipAnimator);
+    flipAnimator = new FlipAnimator();
+    flipAnimator.setDuration(600);
+    flipAnimator.setRepeatCount(-1);
+    flipAnimator.setFillAfter(true);
+    flipAnimator.setInterpolator(new DecelerateInterpolator());
+    view.startAnimation(flipAnimator);
+  }
+
+  public void stopAnimation() {
+    stop = true;
+  }
+
+  public class FlipAnimator extends Animation {
+
+    private Camera camera;
+    private float centerX;
+    private float centerY;
+
+    public FlipAnimator() {
+      setFillAfter(true);
     }
 
-    public void stopAnimation() {
-        stop = true;
+    @Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight) {
+      super.initialize(width, height, parentWidth, parentHeight);
+      camera = new Camera();
+      this.centerX = width / 2;
+      this.centerY = height / 2;
     }
 
-    public class FlipAnimator extends Animation {
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+      // Angle around the y-axis of the rotation at the given time. It is
+      // calculated both in radians and in the equivalent degrees.
+      final double radians = Math.PI * interpolatedTime;
+      float degrees = (float) (180.0 * radians / Math.PI);
 
-        private Camera camera;
-        private float centerX;
-        private float centerY;
-
-        public FlipAnimator() {
-            setFillAfter(true);
+      if (interpolatedTime > 0.4 && interpolatedTime < 0.6) {
+        if (justStarted) {
+          flippingView.setAlpha(1.0f);
+          YoYo.with(Techniques.FadeIn).duration(200).playOn(flippingView);
+          justStarted = false;
+        } else if (stop) {
+          flippingView.setVisibility(View.INVISIBLE);
+          flippingView.clearAnimation();
         }
+      }
 
-        @Override
-        public void initialize(int width, int height, int parentWidth, int parentHeight) {
-            super.initialize(width, height, parentWidth, parentHeight);
-            camera = new Camera();
-            this.centerX = width / 2;
-            this.centerY = height / 2;
-        }
+      final Matrix matrix = t.getMatrix();
 
-        @Override
-        protected void applyTransformation(float interpolatedTime, Transformation t) {
-            // Angle around the y-axis of the rotation at the given time. It is
-            // calculated both in radians and in the equivalent degrees.
-            final double radians = Math.PI * interpolatedTime;
-            float degrees = (float) (180.0 * radians / Math.PI);
+      camera.save();
+      camera.translate(0.0f, 0.0f, (float) (150.0 * Math.sin(radians)));
+      camera.rotateX(true ? degrees : 0);
+      camera.getMatrix(matrix);
+      camera.restore();
 
-            if (interpolatedTime > 0.4 && interpolatedTime < 0.6) {
-                if (justStarted) {
-                    flippingView.setAlpha(255);
-                    YoYo.with(Techniques.FadeIn).duration(200).playOn(flippingView);
-                    justStarted = false;
-                } else if (stop) {
-                    flippingView.setVisibility(View.INVISIBLE);
-                    flippingView.clearAnimation();
-                }
-            }
-
-            final Matrix matrix = t.getMatrix();
-
-            camera.save();
-            camera.translate(0.0f, 0.0f, (float) (150.0 * Math.sin(radians)));
-            camera.rotateX(true ? degrees : 0);
-            camera.getMatrix(matrix);
-            camera.restore();
-
-            matrix.preTranslate(-centerX, -centerY);
-            matrix.postTranslate(centerX, centerY);
-        }
+      matrix.preTranslate(-centerX, -centerY);
+      matrix.postTranslate(centerX, centerY);
     }
+  }
 }
