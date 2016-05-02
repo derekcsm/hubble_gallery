69a9ea19447022d3cd72ae14cb8e6ef5d550b23c
derekcsm <derekcraigsmith@gmail.com>
2016-05-02 13:17:33 -0700

    cleanup NetworkUtil

--- app/src/main/java/com/derek_s/hubble_gallery/utils/network/NetworkUtil.java
+++ app/src/main/java/com/derek_s/hubble_gallery/utils/network/NetworkUtil.java
@@ -9,58 +9,51 @@ import com.derek_s.hubble_gallery.utils.ui.Toasty;
 
 import javax.inject.Inject;
 
-/**
- * Created by dereksmith on 15-04-12.
- */
 public class NetworkUtil {
-    public static int TYPE_WIFI = 1;
-    public static int TYPE_MOBILE = 2;
-    public static int TYPE_NOT_CONNECTED = 0;
+  public static int TYPE_WIFI = 1;
+  public static int TYPE_MOBILE = 2;
+  public static int TYPE_NOT_CONNECTED = 0;
 
-    private final ConnectivityManager connectivityManager;
-    private Context context;
+  private final ConnectivityManager connectivityManager;
+  private Context context;
 
-    @Inject
-    public NetworkUtil(ConnectivityManager connectivityManager, Context context) {
-        this.connectivityManager = connectivityManager;
-        this.context = context;
+  @Inject
+  public NetworkUtil(ConnectivityManager connectivityManager, Context context) {
+    this.connectivityManager = connectivityManager;
+    this.context = context;
+  }
+
+  public int getConnectivityStatus() {
+    NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+    if (null != activeNetwork) {
+      if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+        return TYPE_WIFI;
+
+      if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+        return TYPE_MOBILE;
     }
+    return TYPE_NOT_CONNECTED;
+  }
 
-    public int getConnectivityStatus() {
-        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-        if (null != activeNetwork) {
-            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
-                return TYPE_WIFI;
-
-            if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
-                return TYPE_MOBILE;
-        }
-        return TYPE_NOT_CONNECTED;
+  public String getConnectivityStatusString() {
+    int conn = getConnectivityStatus();
+    String status = null;
+    if (conn == NetworkUtil.TYPE_WIFI) {
+      status = "Wifi enabled";
+    } else if (conn == NetworkUtil.TYPE_MOBILE) {
+      status = "Mobile data enabled";
+    } else if (conn == NetworkUtil.TYPE_NOT_CONNECTED) {
+      status = "Not connected to Internet";
     }
+    return status;
+  }
 
-    public String getConnectivityStatusString() {
-        int conn = getConnectivityStatus();
-        String status = null;
-        if (conn == NetworkUtil.TYPE_WIFI) {
-            status = "Wifi enabled";
-        } else if (conn == NetworkUtil.TYPE_MOBILE) {
-            status = "Mobile data enabled";
-        } else if (conn == NetworkUtil.TYPE_NOT_CONNECTED) {
-            status = "Not connected to Internet";
-        }
-        return status;
-    }
+  public void toastNoConnection() {
+    Toasty.show(context, R.string.no_connection, Toasty.LENGTH_MEDIUM);
+  }
 
-    public void toastNoConnection() {
-        Toasty.show(context, R.string.no_connection, Toasty.LENGTH_MEDIUM);
-    }
-
-    public boolean isConnected() {
-        int conn = getConnectivityStatus();
-        if (conn == NetworkUtil.TYPE_NOT_CONNECTED) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+  public boolean isConnected() {
+    int conn = getConnectivityStatus();
+    return conn != NetworkUtil.TYPE_NOT_CONNECTED;
+  }
 }
